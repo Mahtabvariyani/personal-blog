7b0f760b4627abbcd6fe7637e5f1a28117982e99
Mahtabvariyani <mahtabvariyani@gmail.com>
2023-11-11 15:15:10 +0100

    sanity

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,48 +1,48 @@
 import Link from "next/link";
 import { client } from "../lib/sanity";
+import { Post } from "../lib/interface";
 
 async function getData() {
-  const query = `*[_type == 'post']`;
+  const query = `*[_type == "post"]`;
+
   const data = await client.fetch(query);
 
   return data;
 }
 
-export interface post {
-  title: string;
-  overview: string;
-  content: any;
-  _id: string;
-  slug: {
-    current: string;
-  };
-  _createAt: string;
-}
+export default async function IndexPage() {
+  const data = (await getData()) as Post[];
 
-export default async function posts() {
-  const data = (await getData()) as post[];
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-2xl font-bold leading-9 tracking-tight text-white dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading14 ">
-          AlL Posts
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          All Posts
         </h1>
       </div>
+
       <ul>
         {data.map((post) => (
           <li key={post._id} className="py-4">
             <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+              <div>
+                <p className="text-base font-medium leading-6 text-teal-500">
+                  {new Date(post._createdAt).toISOString().split("T")[0]}
+                </p>
+              </div>
+
               <Link
                 href={`/post/${post.slug.current}`}
                 prefetch
                 className="space-y-3 xl:col-span-3"
               >
                 <div>
-                  <h3 className="text-2xl font-bold leading-8 tracking-tight text-white ">
+                  <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
                     {post.title}
                   </h3>
                 </div>
-                <p className="text-white prose max-w-none line-clam-2">
+
+                <p className="prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-2">
                   {post.overview}
                 </p>
               </Link>
