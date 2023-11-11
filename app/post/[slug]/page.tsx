55ce87903d789a1cb25b6f1cb053870367fe4718
Mahtabvariyani <mahtabvariyani@gmail.com>
2023-11-11 09:52:12 +0100

    fixed

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,8 +1,8 @@
+import { Post } from "@/app/lib/interface";
 import { client } from "@/app/lib/sanity";
+import { urlFor } from "@/app/lib/sanityImageUrl";
 import { PortableText } from "@portabletext/react";
 import Image from "next/image";
-import ImageUrlBuilder from "@sanity/image-url";
-
 
 async function getData(slug: string) {
   const query = `*[_type == "post" && slug.current == "${slug}"][0]`;
@@ -12,27 +12,13 @@ async function getData(slug: string) {
   return data;
 }
 
-export interface post {
-  title: string;
-  overview: string;
-  content: any;
-  _id: string;
-  slug: {
-    current: string;
-  };
-  _createAt: string;
-}
-const builder = ImageUrlBuilder(client);
-
-export function urlFor(source: any) {
-    return builder.image(source);
-  }
 export default async function SlugPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  const data = (await getData(params.slug)) as post;
+  const data = (await getData(params.slug)) as Post;
+
   const PortableTextComponent = {
     types: {
       image: ({ value }: { value: any }) => (
@@ -47,29 +33,28 @@ export default async function SlugPage({
     },
   };
 
-  return(
+  return (
     <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-    <header className="pt-6 xl:pb-6">
-      <div className="space-y-1 text-center">
-   
-        <div>
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-            {data.title}
-          </h1>
+      <header className="pt-6 xl:pb-6">
+        <div className="space-y-1 text-center">
+          <div>
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+              {data.title}
+            </h1>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
 
-    <div className="divide-y divide-gray-200 pb-7 dark:divide-gray-700 xl:divide-y-0">
-      <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-        <div className="prose max-w-none pb-8 pt-10 dark:prose-invert prose-lg">
-          <PortableText
-            value={data.content}
-            components={PortableTextComponent}
-          />
+      <div className="divide-y divide-gray-200 pb-7 dark:divide-gray-700 xl:divide-y-0">
+        <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+          <div className="prose max-w-none pb-8 pt-10 dark:prose-invert prose-lg">
+            <PortableText
+              value={data.content}
+              components={PortableTextComponent}
+            />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
