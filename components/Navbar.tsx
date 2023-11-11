dc577cad10051f36b3724274d69077e672f11e73
Mahtabvariyani <mahtabvariyani@gmail.com>
2023-11-11 10:53:36 +0100

    fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,9 +11,7 @@ const Navbar = () => {
             {navLinks.map((link) => (
               <li key={link.name}>
                 <Link className="hover:text-gray-500" href={`/${link.href}`}>
-                  <button className="text-zinc-400 hover:text-zinc-200 backdrop-blur-lg bg-gradient-to-tr from-transparent via-[rgba(121,121,121,0.16)] to-transparent rounded-md py-2 px-6 shadow hover:shadow-zinc-400 duration-700">
                     {link.name}
-                  </button>
                 </Link>
               </li>
             ))}
