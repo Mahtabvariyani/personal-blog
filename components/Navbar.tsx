fab76e86ba284737e71b1bfc9c3aa0442fb59475
Mahtabvariyani <mahtabvariyani@gmail.com>
2023-11-11 10:31:37 +0100

    fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { navLinks } from "@/constants";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
@@ -9,11 +10,11 @@ const Navbar = () => {
           <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
             {navLinks.map((link,index) => (
               <li key={index}>
-                <a className="hover:text-gray-500" href={link.href}>
+                <Link className="hover:text-gray-500" href={link.href}>
                   <button className="text-zinc-400 hover:text-zinc-200 backdrop-blur-lg bg-gradient-to-tr from-transparent via-[rgba(121,121,121,0.16)] to-transparent rounded-md py-2 px-6 shadow hover:shadow-zinc-400 duration-700">
                     {link.name}
                   </button>
-                </a>
+                </Link>
               </li>
             ))}
           </ul>
