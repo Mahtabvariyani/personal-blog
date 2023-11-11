0caed357bb7a782da173fe2cf24927e346894868
Mahtabvariyani <mahtabvariyani@gmail.com>
2023-11-11 10:42:33 +0100

    fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,27 +8,15 @@ const Navbar = () => {
       <nav className="flex justify-between items-center w-[92%] mx-auto">
         <div className="nav-links duration-500 md:static absolute md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
           <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
-          
-            <li>
-              <Link  href="/posts" >
-                posts
-              </Link>
-            </li>
-            <li>
-              <Link  href="/questions" >
-              questions
-              </Link>
-            </li>
-            <li>
-              <Link  href="/learn" >
-              learn
-              </Link>
-            </li>
-            <li>
-              <Link  href="/learned" >
-              learned
-              </Link>
-            </li>
+            {navLinks.map((link) => (
+              <li key={link.name}>
+                <Link className="hover:text-gray-500" href={link.href}>
+                  <button className="text-zinc-400 hover:text-zinc-200 backdrop-blur-lg bg-gradient-to-tr from-transparent via-[rgba(121,121,121,0.16)] to-transparent rounded-md py-2 px-6 shadow hover:shadow-zinc-400 duration-700">
+                    {link.name}
+                  </button>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
