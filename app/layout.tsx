1e9d4c5a65c943e667fd05edff4eb520c5400bb8
Mahtabvariyani <mahtabvariyani@gmail.com>
2023-11-16 21:15:29 +0100

    removed clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark, neobrutalism } from '@clerk/themes';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
