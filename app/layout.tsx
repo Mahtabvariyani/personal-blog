3c7adcc34fc20ba56715a69adb50cc194fb16bd3
Mahtabvariyani <mahtabvariyani@gmail.com>
2023-12-27 22:03:41 +0100

    new Projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        
         <Navbar />
+   
 
         {children}
         <SpeedInsights />
-        
+
         <Footer />
+     
       </body>
     </html>
   );
--- constants/index.tsx
+++ constants/index.tsx
@@ -49,7 +49,7 @@ export const navLinks = [
   },
   {
     href: "/important",
-    name: "Important",
+    name: "Projects",
   },
   {
     href: "/post",
@@ -77,18 +77,15 @@ export const navLinks = [
   },
 ];
 
-
-
 export const posts = [
   {
     id: 11,
     border: "border-teal-500",
-    bg:'natural',
+    bg: "natural",
     title: "Natural life",
     href: "https://github.com/Mahtabvariyani/NaturalLife",
     description:
-    "NaturalLife Store is a web application that allows a family (Farm FamilyHack) in a village to sell their products online. Each season brings different offers and products for users to browse and order. The website also provides weather information using an API and allows both registered and non-registered users to contact the family.",
-
+      "NaturalLife Store is a web application that allows a family (Farm FamilyHack) in a village to sell their products online. Each season brings different offers and products for users to browse and order. The website also provides weather information using an API and allows both registered and non-registered users to contact the family.",
 
     category: { title: "Live", href: "#" },
     author: {
@@ -100,12 +97,11 @@ export const posts = [
   {
     id: 12,
     border: "border-teal-500",
-    bg:'api',
+    bg: "api",
     title: "API",
     href: "https://labtop-blog.vercel.app/",
     description:
-    "An interconnected web application with diverse pages, each linked to various APIs for comprehensive testing and functionality. Explore different API integrations across each page, showcasing the dynamic interaction with varied external services.",
-
+      "An interconnected web application with diverse pages, each linked to various APIs for comprehensive testing and functionality. Explore different API integrations across each page, showcasing the dynamic interaction with varied external services.",
 
     category: { title: "Live", href: "#" },
     author: {
@@ -115,14 +111,13 @@ export const posts = [
     },
   },
   {
-    id: 1,
+    id: 13,
     border: "border-teal-500",
-    bg:'wecode',
+    bg: "wecode",
     title: "We Code",
-    href:       "https://we-dev.vercel.app/sign-up?redirect_url=https%3A%2F%2Fwe-dev.vercel.app%2F",
+    href: "https://we-dev.vercel.app/sign-up?redirect_url=https%3A%2F%2Fwe-dev.vercel.app%2F",
     description:
-    "ConnectDev is a collaborative platform designed for developers to connect, communicate, collaborate, and code together. It offers a space for developers to ask questions, share knowledge, and work on coding projects in real-time. With built-in authentication powered by Clerk, users can create or join communities of up to 5 members to work together seamlessly.",
-
+      "ConnectDev is a collaborative platform designed for developers to connect, communicate, collaborate, and code together. It offers a space for developers to ask questions, share knowledge, and work on coding projects in real-time. With built-in authentication powered by Clerk, users can create or join communities of up to 5 members to work together seamlessly.",
 
     category: { title: "Live", href: "#" },
     author: {
@@ -132,14 +127,13 @@ export const posts = [
     },
   },
   {
-    id: 1,
+    id: 14,
     border: "border-teal-500",
-    bg:'bg-Aapi',
+    bg: "bg-Aapi",
     title: "Renting App",
     href: "https://carrenting-app.vercel.app/",
     description:
-    "This is a small Renting app that is connected to the Api so the user can search and find a car to rent ",
-
+      "This is a small Renting app that is connected to the Api so the user can search and find a car to rent ",
 
     category: { title: "Live", href: "#" },
     author: {
@@ -149,57 +143,67 @@ export const posts = [
     },
   },
   {
-    id: 1,
+    id: 16,
     border: "border-teal-500",
-    bg:'ski',
+    bg: "ski",
     title: "Ski Wear Shop",
     href: "https://skiwearshop.vercel.app/",
     description:
-    "A Small Commerce App without Authentication for justy trying and testing the App having a Stripe testing payment and adding the Products and APi  with Sanity",
-
+      "A Small Commerce App without Authentication for justy trying and testing the App having a Stripe testing payment and adding the Products and APi  with Sanity",
 
     category: { title: "Live", href: "#" },
     author: {
       name: "Ski-Wear app",
       role: "Github Source Code",
       href: "https://github.com/Mahtabvariyani/skiwearshop",
-    
     },
   },
   {
-    id: 1,
+    id: 17,
     border: "border-teal-500",
-    bg:'fendous',
+    bg: "fendous",
     title: "Fendous Website",
     href: "https://chat-bot-landing-page.vercel.app/",
     description:
-    "A Website for Fendous Company in Denmark which  i  created it  according to their needs and requests",
-
+      "A Website for Fendous Company in Denmark which  i  created it  according to their needs and requests",
 
     category: { title: "Live", href: "#" },
     author: {
       name: "Fendous App",
       role: "Github Source Code",
       href: "https://github.com/Mahtabvariyani/Chat-bot-Landing-Page",
-
     },
   },
   {
-    id: 1,
+    id: 18,
     border: "border-teal-500",
-    bg:'bg-Quiz',
+    bg: "bg-Quiz",
     title: "Quiz App",
     href: "https://coding-quiz-nine.vercel.app/",
     description:
-    "few Days back i was trying to Find an app or a website to do few Quiz about JavaScript or TypeScript or ... i didnt find so i made it myself (Its still not Completed im working on it)",
-
+      "few Days back i was trying to Find an app or a website to do few Quiz about JavaScript or TypeScript or ... i didnt find so i made it myself (Its still not Completed im working on it)",
 
     category: { title: "Live", href: "#" },
     author: {
       name: "Quiz App",
       role: "Github Source Code",
       href: "https://github.com/Mahtabvariyani/CodingQuiz",
-     
+    },
+  },
+  {
+    id: 18,
+    border: "border-teal-500",
+    bg: "bg-Quiz",
+    title: "IT Event App",
+    href: "https://mah-event-app.vercel.app/",
+    description:
+      "an App for the Developers so they can make an Event and make their own price and then even buying the other events , and there is also posts so they can read Related",
+
+    category: { title: "Live", href: "#" },
+    author: {
+      name: "IT Event App",
+      role: "Github Source Code",
+      href: "https://github.com/Mahtabvariyani/eventApp",
     },
   },
 ];
