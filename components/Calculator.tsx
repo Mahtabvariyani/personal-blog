6996ec8995ab5abbe367eb63f0234312f6027cfd
Mahtabvariyani <mahtabvariyani@gmail.com>
2023-12-30 16:22:28 +0100

    new style

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
+import Lottie from "lottie-react";
+import React from "react";
+import finding from "@/public/finding.json";
 
 const Calculator = () => {
   return (
-    <div>Calculator</div>
-  )
-}
+    <div className="pt-20 -mb-10">
+      <Lottie animationData={finding} className="h-[20rem]" />
+    </div>
+  );
+};
 
-export default Calculator
+export default Calculator;
