493f72e6174363d15a80fb93cf40f52edb68e982
Mahtabvariyani <mahtabvariyani@gmail.com>
2024-01-05 17:29:33 +0100

    new fixes

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,14 +1,25 @@
 "use client";
-import Lottie from "lottie-react";
-import React from "react";
-import finding from "@/public/finding.json";
+import CatData from "./CatData";
 
-const Calculator = () => {
+export default function Calculator() {
   return (
-    <div className="pt-20 -mb-10">
-      <Lottie animationData={finding} className="h-[20rem]" />
+    <div className="mt-3 -mb-[30vh] py-24 sm:py-32 apipic">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 ">
+          <div className="flex flex-col">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">
+              Dummy API Data
+            </h2>
+            <p className="mt-2 text-lg leading-8 text-gray-100">
+              Click Each Button To see The dummy Data
+            </p>
+          </div>
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <CatData />
+        <CatData /><CatData />
+        </div>
+      </div>
     </div>
   );
-};
-
-export default Calculator;
+}
