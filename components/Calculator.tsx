97fd567285478aa077511f10e21a14feca69ac03
Mahtabvariyani <mahtabvariyani@gmail.com>
2024-01-07 15:37:07 +0100

    Fixed the back ground

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -15,7 +15,7 @@ export default function Calculator() {
             </p>
           </div>
         </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-10 flex max-w-2xl justify-between grid-cols-1 gap-x-8 gap-y-16 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         <ActivityData />
         <JokeData />
         </div>
