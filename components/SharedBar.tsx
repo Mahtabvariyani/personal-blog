8126420d482a27eb9ac64b3719f2b3a13413d53e
Mahtabvariyani <mahtabvariyani@gmail.com>
2024-01-07 13:45:14 +0100

    fixed the share bar

--- components/SharedBar.tsx
+++ components/SharedBar.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { GiFireBottle } from "react-icons/gi";
+import { BsInfoSquare } from "react-icons/bs";
 
 import {
   Sheet,
   SheetContent,
+  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -12,27 +13,21 @@ import {
 import Lottie from "lottie-react";
 import Bar from "@/public/bar.json";
 import Link from "next/link";
-import InterviewQuestion from "@/components/Questions";
 const links = [
   {
-    href: "#",
-    text: "Make changes to your profile here. Click save when you're done.",
-    id: 1,
+    href: "/javaScript",
+    text: "JavaScript Interview Questions ",
+    id: 11,
   },
   {
-    href: "#",
-    text: "Make changes to your profile here. Click save when you're done.",
+    href: "/react",
+    text: "React Interview Questions ",
     id: 12,
   },
   {
-    href: "#",
-    text: "Make changes to your profile here. Click save when you're done.",
-    id: 13,
-  },
-  {
-    href: "#",
-    text: "Make changes to your profile here. Click save when you're done.",
-    id: 14,
+    href: "https://coding-quiz-nine.vercel.app/",
+    text: "JavaScript, TypeScript, React, Nextjs and etc ... Quiz Game",
+    id: 12,
   },
 ];
 export function SheetDemo() {
@@ -40,15 +35,26 @@ export function SheetDemo() {
     <Sheet>
       <SheetTrigger asChild>
         <div className="fixed top-0 right-0 mt-[60vh] mr-4 bg-transparent border-none ">
-          <GiFireBottle
-            size={40}
-            className="text-sky-600 gradient-background"
+          <BsInfoSquare
+            size={30}
+            className="text-sky-100 blink"
           />
         </div>
       </SheetTrigger>
       <SheetContent className="bg-transparent backdrop-blur-xl text-white border-left border-sky-700">
         <SheetHeader className="text-white">
           <SheetTitle className="text-sky-700 italic">More Infos</SheetTitle>
+          <SheetDescription>
+            {links.map((link) => (
+              <Link
+                href={link.href}
+                key={link.id}
+                className="flex flex-col text-white mt-4 underline hover:text-sky-700 duration-700"
+              >
+                {link.text}
+              </Link>
+            ))}
+          </SheetDescription>
 
           <div className="flex flex-col py-4">
             <div className="flex flex-col items-center gap-4">
